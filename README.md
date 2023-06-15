6716051fab6477caf6ad4e170d80e651055e236e
tlholo-kvs <tlholokgoele@hotmail.com>
2023-06-15 14:17:17 +0200

    feat(setup): added new screen and updated README

--- README.md
+++ README.md
@@ -3,11 +3,6 @@
 Anime Directory is a simple application that allows a user to keep  track of all their favourite animes
 and it's associated metadat
 
-## Contributing
 
-Pull requests are welcome. For major changes, please open an issue first
-to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
 
 
---
+++ lib/screens/add_anime_title.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNewAnime extends StatefulWidget {
+  const AddNewAnime({super.key});
+
+  @override
+  State<AddNewAnime> createState() => _AddNewAnimeState();
+}
+
+class _AddNewAnimeState extends State<AddNewAnime> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
