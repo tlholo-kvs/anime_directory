96105f9e18ccb7f3e55d1b038dd5a8a8a95648fa
tlholo-kvs <tlholokgoele@hotmail.com>
2023-06-15 14:16:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Anime Directory
+
+Anime Directory is a simple application that allows a user to keep  track of all their favourite animes
+and it's associated metadat
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+
---
+++ lib/screens/login_register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginRegister extends StatefulWidget {
+  const LoginRegister({super.key});
+
+  @override
+  State<LoginRegister> createState() => _LoginRegisterState();
+}
+
+class _LoginRegisterState extends State<LoginRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
